12b6deb9de0295fcb0762079ca18e9a45d6a5787
Kofsaku <k_tsubata@azoop.co.jp>
2021-12-16 21:33:06 +0900

    製品一覧を表示させるところまで完成

--- pages/_document.js
+++ pages/_document.js
@@ -17,21 +17,20 @@ export default class MyDocument extends Document {
 
 }
 
-
-
 MyDocument.getInitialProps = async (ctx) => {
-  const sheets = new ServerStyleSheets()
-  const originalRenderPage = ctx.renderPage
+  const sheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
   ctx.renderPage = () => {
-  return originalRenderPage({
-    enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
-  })
- }
- const initialProps = await Document.getInitialProps(ctx)
- return {
-   ...initialProps,
-   styles: [
-     ...React.Children.toArray(initialProps.styles),
-   ]
- }
-}
+    return originalRenderPage({
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    });
+  };
+  const initialProps = await Document.getInitialProps(ctx);
+  return {
+    ...initialProps,
+    styles: [
+      ...React.Children.toArray(initialProps.styles),
+      sheets.getStyleElement(),
+    ],
+  };
+};
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,37 @@
-import { dividerClasses } from '@mui/material'
-import { spacing } from '@mui/system';
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { CardActionArea, CardActions, CardContent, CardMedia, dividerClasses, Typography, Button, Grid, Card } from '@mui/material'
 import Layout from '../components/Layout'
+import data from '../utils/data';
 
 export default function Home() {
   return (
     <Layout>
       <div>
-        <h1>Products</h1>
+        <h1>製品一覧</h1>
+        <Grid container spacing={3}>
+          {data.products.map((product) => (
+            <Grid item md={4} key={product.name}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    image={product.image}
+                    title={product.name}>
 
-
-        <ul>
-          <li>Product1</li>
-          <li>Product1</li>
-          <li>Product1</li>
-        </ul>
+                  </CardMedia>
+                  <CardContent>
+                    <Typography>
+                      {product.name}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Typography>${product.price}</Typography>
+                  <Button size="small" color="primary">Add to Cart</Button>
+                </CardActions>
+              </Card>
+            </Grid>
+            ))}
+        </Grid>
       </div>
     </Layout>
 
